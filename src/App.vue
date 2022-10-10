fe2cd8bda5cf86e3394f7d784039b862465a9b37
Luis <luisaugustos@usal.es>
2022-10-10 22:24:48 +0200

    pushing all to git

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app id="upwork-design">
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
+
+<style>
+#upwork-design {
+  background-color: #E5E5E5;
+ }
+ </style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,313 @@
+<!-- Made by luisaugustos -->
+
+<template>
+  <div class="hello">
+    <div class="header">
+       <p>We make creative media <br> that <span> adds value</span></p>
+    </div>
+    <div class="item-list">
+      <v-container fluid>
+
+        <v-row>
+          <v-col cols="4">
+            <v-card class="item" flat>
+              <v-row>
+                <v-col cols="4" class="item-img">
+                  <div class="img-top">
+                    <img class="img-card-double" style="width: 120%;" src="https://s3-alpha-sig.figma.com/img/feaa/a64a/97de99725043b78ceae7a3854fcb9889?Expires=1666569600&Signature=CGOeLNr8RFwe~vmqxZF39wwXDnnmYFsiwvuhB3cQWbvSXP9Llo8gcpoJtZoiscY9kATL5sBfvOoGnKlBFbzBYyJdJeJTFD0sKVGgB4UJ1m8MPoLQ7GiSoaVMSSLsiuLnKiVNGvx81Ja4c7qSfqcmnbvLkETwM55qN1arbrdXcn5~YMndpYOyz9EvocFZqX2vs5GP0MQOEM9Weu4CyI7hmyPHpOFH40~x9tkpHdxoiEcJsO-LwezFj04C6vd8cqhM3KnZvBgEOMBYqSATeNsqCETjLDF3HA8ufWW3KJBM2pUMtcwvt5KxLBD9yqVbT8UtNZzjlZeUCMjzXskqK6OUIg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
+                  </div>
+                  <div class="img-bottom" style="margin-bottom: 6px;margin-right: -12px;overflow: hidden;">
+                    <img class="img-card-double" style="width: 120%; margin: -27px 0px 0px 0px;" src="https://s3-alpha-sig.figma.com/img/2fe4/3f0f/a87eb30d4566c0282c273b0ddab1d8ac?Expires=1666569600&Signature=L0x2YrdYmG48R07RpJN8jnvjS8vk1Kmjbyn7vNkgYFcwRiPqoEmLmpTBj5cNcC7OXKI2j7Va9VdE440mXUjZpjX7qNK5X6sp~F17bm2BbZVaKrhNX7jNru43LuhT-VoG~sO2UKblp7RwdZLFG2i4T53p1DphBZR8Y6b8fR7wsxrCjykKIQyWLlbq104Krj0-4h~KjjO1xP59UaCH6rjrsgkDZeB3yLmdduA~ROjCM4zX~-aaMBw3u~ALLAmBwcF~rPvRBPpVo8sEmKRGgrN0CX92SUjPDMyZZA9sd~fNMTuQq6wSytk5ytyJOuadspRB4gZDQ5M2tTXRmJz728Eafw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
+                  </div>
+                </v-col>
+                <v-col cols="8">
+                  <div class="details">
+                    <h3>Photography</h3>
+
+                    <ul>
+                      <li>Drone Photography</li>
+                      <li>Interiors</li>
+                      <li>Exteriors / Architectural</li>
+                    </ul>
+                    
+                    <p>
+                      Every photograph we produce finds the beauty in your property while serving as a profitable asset.
+                    </p>
+                  </div>
+                </v-col>
+
+              </v-row>              
+            </v-card>
+          </v-col>
+
+          <v-col cols="4">
+            <v-card class="item" flat>
+              <v-row>
+                <v-col cols="4" class="item-img">
+                    <img class="img-card" style="transform: translateX(-36%);" src="https://s3-alpha-sig.figma.com/img/0713/8cf3/11224ae4e0cbfc975b0dcb24e41019ca?Expires=1666569600&Signature=Zza5T-i6FeGaK2iW3QJ5X0NcVM3nUvMgMj05KW8~4hBVVSAodnaxS9wRgPRb4u6XPNJ5-xGgEoAtqoYC8SDfSLsC1ubM7IDsWgsA6qIKS3GmP1g~vJiN3ARhFteFTm6EdwIpvqE~BadbltpHpsrWHBySI030j1jeVkpqVFhwrtl6TAYXm15SW1W~No8iSWpbHzr3cYRDgPGrKz9E2PUvK9kBi8BntR8--ir5dLULyhBcc0p-iyrA5eNfb52BL2Jm1DWRTGFBpwHj6jIAMSjBIyX6SXPrXdEXho~C8EegajcCer6acfKcpfQ9BloWC-Mm6kcOp7gQ9gRZb9mFNDfCGg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
+                </v-col>
+                <v-col cols="8">
+                  <div class="details">
+                    <h3>Virtual Staging</h3>
+                    <p>
+                      Our staging will help you sell the potential of your space.
+                    </p>
+                    <p>
+                      We can work off of our own photography or photos you provide.
+                    </p>
+                  </div>
+                </v-col>
+
+              </v-row>              
+            </v-card>
+          </v-col>
+
+          <v-col cols="4">
+            <v-card class="item" flat>
+              <v-row>
+                <v-col cols="4" class="item-img">
+                  <img class="img-card" src="https://s3-alpha-sig.figma.com/img/582a/2296/580f817bbb8ec9f4817727977634c513?Expires=1666569600&Signature=AuIEVHOjLcGP0m234i4A-FYYDi1dXqOKiRBswehfj5Z~pIrug7~da6yW93FWwvHDyN0vC22zoNs7-QlALEdTdeogqWOtQjbK8XN6y2DEIQzdb5fcwzUp7YcekU3vTtXdIRZvLspvdC1odKh2X1xgbPUDOctBkc6PPnDAvhdbvO~6HeCpiihRedNFjSUivExwvrNXgYfPqJ03VbUv3iWq2c1VQnlaPDfavcL9ZNkrnOwYnEVs4-4luSiSr95GoCL44DwSc3UmFzikSb-OtLPR6KlMRpDh3GLFowwCaa~EKh0dHPGoAdyBjPzHooY4RfmEBXwqJKwVwroj1LzXrFfDSQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
+                </v-col>
+                <v-col cols="8">
+                  <div class="details">
+                    <h3>Renderings</h3>
+                    <ul>
+                      <li>Interiors</li>
+                      <li>Exteriors</li>
+                    </ul>
+                    <p>
+                      Renderings are the gold standard in pre-construction marketing.
+                    </p>
+                  </div>
+                </v-col>
+
+              </v-row>              
+            </v-card>
+
+          </v-col>
+        </v-row>
+        
+        <v-spacer></v-spacer>
+
+        <v-row style="margin-top: 28px;">
+
+          <v-col cols="4">
+            <v-card class="item" flat>
+              <v-row>
+                <v-col cols="4" class="item-img">
+                  <img class="img-card" src="https://s3-alpha-sig.figma.com/img/93dd/eaf9/29fd1627b885d0cdc50ead4e4a6be598?Expires=1666569600&Signature=HUiNWGmHmdsj20dS~HPI2soBhTglMJPDdD0DGmJiHNnz-cuXTvCKKpNlAeX3OmyvcOyWc0kCav5-G82Pa6FHBbOWJ2VkWtd0uDTcoQwmsv5kivR11BdQAFWe5nweFMLGC3t6AWpVxeepTCudK~vwZesQ5HS4Oy0qT6K6qlgTqX-KDQUBvUJgEI4-QjN92ceNCktec0LhbQm1y6T6skYyfH9KCiJJw6q3AHTOr7RyjmOInlkNl78z-I9gU~I~G8V-ni~2OXu~zP23AlyWKHhFQf82lvpzrlZbIyYkap8yFjDrs7~pAtAAFDeUHKOyKjXfW~JFozSJSSdsl7mcMsdyBw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt="">
+                </v-col>
+                <v-col cols="8">
+                  <div class="details">
+                    <h3>Interactive Tours</h3>
+
+                    <ul>
+                      <li>Matterport</li>
+                      <li>Video Walkthroughs</li>
+                    </ul>
+                    
+                    <p>
+                      Give your audience the ability to explore your property without requiring in-person travel.
+                    </p>
+                  </div>
+                </v-col>
+
+              </v-row>              
+            </v-card>
+          </v-col>
+
+          <v-col cols="4">
+            <v-card class="item" flat>
+              <v-row>
+                <v-col cols="4" class="item-img">
+                  <img class="img-card" src="https://s3-alpha-sig.figma.com/img/8b9a/48e0/3518536eec68cba34cc54bc6f74a5fd5?Expires=1666569600&Signature=OUORupnukO9ouorBnOVovhVG9xBQ4233HTdXQlqCkXFJXAJVDc68WUWogJDgt-FrdhxxgcPu~~w5qa0uD5x7dFWQ0YJDYKNz8dnuS~WIhFv6ZOvo1Y0AZ-7Iou1jRH~hLJk26G-nP88xXJrLYJJdoUtavs17fRoiQRrIgu9iRW7HCTM9mrTjbbDZyc4VimqRoljBN7ifmHt-mhzxbZg1R-6og783Gr~Pf8TCOTNVrsh7AvV7xeE11AoPfZImn6QpWWZePzqdHNYoTFKqcFr~ArDSBwmVybpTunWwjzjCUfdZ0~dm-JBFiM2mvCZTRWT6wrotmNtccJssu5yKH~PzCA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
+                </v-col>
+                <v-col cols="8">
+                  <div class="details">
+                    <h3>Floor Plans</h3>
+                    <ul>
+                      <li>On-site Measure</li>
+                      <li>2D Floor Plans</li>
+                      <li>3D Floor Plans</li> 
+                    </ul>
+                    <p>
+                      We produce laser-precise floor plans faster than any other service.
+                    </p>
+                  </div>
+                </v-col>
+
+              </v-row>              
+            </v-card>
+          </v-col>
+
+          <v-col cols="4">
+            <v-card class="item" flat>
+              <v-row>
+                <v-col cols="4" class="item-img">
+                  <img class="img-card" style="transform: translateX(-66%);" src="https://s3-alpha-sig.figma.com/img/a24f/e96c/5d9cb0d5dcf8bf5c4503ac8b24a7bcfe?Expires=1666569600&Signature=A8S~JgVFLf7vD9KOYDII5D1pF7XGQKSQMzyJSqDC-Hk2m-1LzV6Ey6nCXreKX0Fu7-QqwkRfWcB3JyIWB3GoqvzUgSh4UuJSGoHSXhKsFkXnAMf5rQrUxORI1ICHZS3ozwpu9OS-IMFLmd59roSCscXdRV0Qz1e2uoYwNwPQ~6TfYOOebUn5cT-jvl-LrwmFqd1hiKv5oTt7t~rjCfyEba~lI0TWz3j2ZZoxWECYlz-tBQmefJ0utp0ZGgY~-5pBOCy3Z5oEVO8-fJconp12AjLYTX0eJwvnIvcnKoAbwTEvkDgqoOCb8QAUE4fhGOkyw3AcEvp7gOT1Z3qBOaedAg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
+                </v-col>
+                <v-col cols="8">
+                  <div class="details">
+                    <h3>Video</h3>
+                    <ul>
+                      <li>Drone Cinematography</li>
+                      <li>Interiors</li>
+                      <li>Exteriors / Architectural</li>
+                    </ul>
+                    <p>
+                      Video content has higher engagement and will help your property stand out online.
+                    </p>
+                  </div>
+                </v-col>
+
+              </v-row>              
+            </v-card>
+
+          </v-col>
+
+        </v-row>
+        <div class="text-center">
+          <v-btn color="red lighten-2">
+            GET STARTED
+          </v-btn>
+        </div>
+      </v-container>
+    </div>
+
+
+    
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.hello {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 108px;
+  padding: 0px;
+  .header {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+    p {
+      font-family: 'Poppins'; 
+      font-style: normal;
+      font-weight: 700;
+      font-size: 56px;  
+      line-height: 55px;
+      letter-spacing: -0.01em;
+      color: #1B264F;
+      text-align: center;
+      span {
+        color: #506BCA
+      }
+    }
+  }
+
+}
+
+.item-list {
+  margin-top: 80px;
+  .item {
+    background: #FFFFFF;
+    border-radius: 8px;
+    .item-img {
+      overflow: hidden;
+      text-align: center;
+      padding-bottom: 0;
+      .img-card {
+        max-height: 335px;
+        margin-left: 50%;
+        transform: translateX(-50%);
+      }
+      .img-top {
+        max-height: 167.5px;
+      }
+      .img-bottom {
+        max-height: 167.5px;
+      }
+      .img-card-double {
+        width: 100%;
+      }
+    }
+    
+    .details {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 40px 30px 34px 34px;
+      color: #353844;
+      ul {
+        list-style: none;
+        margin-bottom: 12px;
+      }
+      h3 {
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 32px;
+        margin-bottom: 12px;
+      }
+      li {
+        font-family: 'Nunito Sans';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 15px;
+        line-height: 26px;
+        font-feature-settings: 'salt' on, 'liga' off;
+        margin: 0 10px;
+      }
+      li::before { 
+        content: "\2022";
+        font-weight: bold; 
+        display: inline-block; 
+        width: 1em; 
+        margin-left: -1em;
+        color: #D2AD81;
+
+      }
+      p {
+        font-family: 'Nunito Sans';
+        font-style: normal;
+        font-weight: 600;
+        font-size: 15px;
+        line-height: 24px;
+        font-feature-settings: 'salt' on, 'liga' off;
+      }
+    }
+  }
+}
+
+button {
+  margin-top: 58px;
+  background-color: #1B264F !important;
+  border-radius: 12px;
+  width: 315px;
+  height: 60px !important;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 60px;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.2em;
+  text-transform: uppercase;
+}
+
+</style>
